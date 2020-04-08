a99cf12e9cb425c10411c2ddabfc149ac981f213
mpoemsl <37959974+mpoemsl@users.noreply.github.com>
2020-04-08 12:34:44 +0200

    Added  barplots

--- visualize.py
+++ visualize.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -8,16 +9,54 @@ import numpy as np
 
 def main():
 
+    print("Making heatmaps ...")
     make_heatmaps()
+
+    print("Making barplots ...")
+    make_barplots()
 
 
 def make_barplots():
+
+    colors = {
+        "greedyUncoop": "blue",
+        "stochasticUncoop": "yellow",
+        "stochasticCoop": "green",
+        "greedyCoop": "red"
+    }
     
     for kind in ["pragmatic", "hyperpragmatic"]:
 
         print("Operating on {} mastermind ...".format(kind))
 
         stats = pd.read_csv("{}_strategy_stats.csv".format(kind), index_col=0)
+        patches = [mpatches.Patch(color=c, label=s) for s, c in colors.items()]
+
+        fig, axes = plt.subplots(4, 4, squeeze=True, figsize=(8, 8))
+
+        l_base_pos = [(0, 0), (0, 2), (2, 0), (2, 2)]
+        s_add_pos = [(0, 0), (0, 1), (1, 0), (1, 1)]
+
+        for s_ix, s_strat in enumerate(stats["speakerStrategy"].unique()):
+            for l_ix, l_strat in enumerate(stats["listenerStrategy"].unique()):
+
+                base_x, base_y = l_base_pos[l_ix]
+                add_x, add_y = s_add_pos[s_ix]
+                ax = axes[base_x + add_x, base_y + add_y]
+                
+                ax.bar([0.5, 1.5, 2.5], stats.iloc[s_ix * 4 + l_ix][["mean_utt_some", "mean_utt_many", "mean_utt_none"]], color=colors[s_strat])
+                ax.set_xticks([0.5, 1.5, 2.5])
+                ax.set_xlim(0.0, 3.0)
+                ax.set_ylim(0, 2)
+                ax.set_xticklabels(["some", "many", "none"])
+                ax.set_title(l_strat)
+                
+        plt.suptitle("{} Mastermind\nMean Utterance Frequencies by Listener Strategy\n".format(titlelize(kind)))
+        plt.tight_layout(pad=3.0)
+        plt.subplots_adjust(top=0.85, bottom=0.25)
+        fig.legend(handles=patches, loc="lower center", title="Speaker Strategy")
+
+        plt.savefig("plots/utterances_barplots_{}.png".format(kind.lower()))
 
 
 
@@ -41,7 +80,7 @@ def make_heatmaps():
 
             plt.tight_layout()
 
-            plt.savefig("plots/{}_{}.png".format(value, kind))
+            plt.savefig("plots/{}_{}.png".format(value.lower(), kind.lower()))
             plt.clf()
 
 
@@ -74,7 +113,5 @@ def condense_strategies(df, vcol):
     
 
     
-
-
 if __name__ == "__main__":
     main()
